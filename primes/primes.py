3d15e0adfeca2d35708a6b1d57c0728be6561636
Jimmy Kamau <jimmykambiz@gmail.com>
2016-06-14 15:48:34 +0300

    Added .gitignore and added tests for prime-cheking functions 	new file:   .gitignore 	new file:   primes/primes.py

---
+++ primes/primes.py
@@ -0,0 +1,45 @@
+def prime(i, primes):
+    for prime in primes:
+        if not (i == prime or i % prime):
+            return False
+    primes.append(i)
+    return i
+    
+def get_primes(n):
+	
+	# Validate input
+	if n <= 0:
+		return "Invalid number"
+	
+	primes = [] # Empty list to hold prime numbers
+	i, p = 2, 0 # i=first prime number, p=number of prime numbers found
+	while True:
+	    if prime(i, primes):
+	        p += 1
+	        if p == n:
+	            return primes
+	    i += 1
+
+nth = 50 # 50 prime numbers
+primes_list = get_primes(nth) # Get first 50 prime numbers
+print(primes_list) 
+
+# Begin tests
+'''
+def test_primes(n, prime_list):
+ 	# Generate list of prime numbers using sieve algorithm
+    sieve = [True] * n
+    for i in xrange(3,int(n**0.5)+1,2):
+        if sieve[i]:
+            sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
+    new_primes = [2] + [i for i in xrange(3,n,2) if sieve[i]]
+
+    if new_primes != prime_list:
+    	return "Failed"
+    else:
+    	return "Passed"
+
+last_prime = primes_list[-1]+1
+print (test_primes(last_prime, primes_list))
+'''
+# End tests
