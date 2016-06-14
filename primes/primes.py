5162fe6e6949c156003021f8f4c4a1ca1618314e
Jimmy Kamau <jimmykambiz@gmail.com>
2016-06-14 16:04:36 +0300

    Added tests for functions

--- primes/primes.py
+++ primes/primes.py
@@ -24,7 +24,7 @@ nth = 50 # 50 prime numbers
 primes_list = get_primes(nth) # Get first 50 prime numbers
 print(primes_list) 
 
-# Test prime-checking functions
+# Test for prime-checking functions
 '''
 def test_primes(n, prime_list):
  	# Generate list of prime numbers using sieve algorithm
